6df426091a763c6e76e3b0820f7ca297f8ecaaa3
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2025-09-01 16:45:53 +0530

    feat: modify evictor to handle non-ready allocated frames

--- src/storage/buffer/evict.rs
+++ src/storage/buffer/evict.rs
@@ -1,10 +1,12 @@
-use crate::storage::{buffer::buffer_pool::Frame, page::page_base};
+use crate::storage::buffer::buffer_pool::Frame;
 
-pub trait Evictor {
-    fn pick_victim(&mut self) -> Option<usize>;
+pub trait Evictor<'a> {
+    fn pick_victim(&mut self) -> Option<&'a Frame>;
 
-    fn notify_frame_read(&mut self, frame: &Frame);
-    fn notify_pin(&mut self, frame: &Frame);
-    fn notify_unpin(&mut self, frame: &Frame);
-    fn notify_eviction(&mut self, page_id: page_base::PageId);
+    fn notify_frame_alloc(&mut self, frame: &'a Frame);
+    fn notify_frame_load(&mut self, frame: &'a Frame);
+    fn notify_frame_flush(&mut self, frame: &'a Frame);
+    fn notify_frame_pin(&mut self, frame: &'a Frame);
+    fn notify_frame_unpin(&mut self, frame: &'a Frame);
+    fn notify_frame_destroy(&mut self, frame_id: u32);
 }
--- src/storage/buffer/fifo_evictor.rs
+++ src/storage/buffer/fifo_evictor.rs
@@ -1,72 +1,58 @@
-use crate::storage::{
-    buffer::{Evictor, Frame},
-    page::page_base,
-};
-use std::collections::{HashSet, VecDeque};
+use crate::storage::buffer::{Evictor, Frame};
+use std::collections::{HashMap, HashSet, VecDeque};
 
-pub struct FifoEvictor {
-    victim_queue: VecDeque<page_base::PageId>,
-    pinned: VecDeque<bool>,
-    pages_tracked: HashSet<page_base::PageId>,
+pub struct FifoEvictor<'a> {
+    victim_queue: VecDeque<&'a Frame>,
+    fid_idx_map: HashMap<u32, usize>,
+    frames_tracked: HashSet<u32>,
+    marked_for_eviction: HashSet<u32>,
 }
 
-impl FifoEvictor {
+impl<'a> FifoEvictor<'a> {
     pub fn new() -> Self {
         Self {
             victim_queue: VecDeque::new(),
-            pinned: VecDeque::new(),
-            pages_tracked: HashSet::new(),
+            fid_idx_map: HashMap::new(),
+            frames_tracked: HashSet::new(),
+            marked_for_eviction: HashSet::new(),
         }
     }
 }
 
-impl Evictor for FifoEvictor {
-    fn pick_victim(&mut self) -> Option<usize> {
+impl<'a> Evictor<'a> for FifoEvictor<'a> {
+    fn pick_victim(&mut self) -> Option<&'a Frame> {
         for i in 0..self.victim_queue.len() {
-            if !self.pinned[i] {
-                return Some(i);
+            let frame = self.victim_queue[i];
+            // no need to check is_ready because there wont be any non-ready frames
+            // in the victim queue
+            if !frame.pinned() && !self.marked_for_eviction.contains(&frame.fid()) {
+                self.marked_for_eviction.insert(frame.fid());
+                self.victim_queue.remove(i);
+                return Some(frame);
             }
         }
         None
     }
 
-    fn notify_frame_read(&mut self, frame: &Frame) {
-        let frame_page_id = frame.page_id();
-        if self.pages_tracked.contains(&frame_page_id) {
+    // Frames that are not loaded into wont be queued for eviction
+    fn notify_frame_alloc(&mut self, frame: &Frame) {}
+
+    fn notify_frame_load(&mut self, frame: &'a Frame) {
+        let frame_page_id = frame.fid();
+        if self.frames_tracked.contains(&frame_page_id) {
             return;
         }
-        self.victim_queue.push_back(frame_page_id);
-        self.pinned.push_back(false);
-        self.pages_tracked.insert(frame_page_id);
+        self.victim_queue.push_back(frame);
+        self.frames_tracked.insert(frame.fid());
     }
 
-    fn notify_pin(&mut self, frame: &Frame) {
-        let frame_page_id = frame.page_id();
-        for i in 0..self.victim_queue.len() {
-            if self.victim_queue[i] == frame_page_id {
-                self.pinned[i] = true;
-                break;
-            }
-        }
-    }
+    fn notify_frame_flush(&mut self, frame: &Frame) {}
 
-    fn notify_unpin(&mut self, frame: &Frame) {
-        let frame_page_id = frame.page_id();
-        for i in 0..self.victim_queue.len() {
-            if self.victim_queue[i] == frame_page_id {
-                self.pinned[i] = false;
-                break;
-            }
-        }
-    }
+    fn notify_frame_pin(&mut self, frame: &Frame) {}
 
-    fn notify_eviction(&mut self, page_id: page_base::PageId) {
-        for i in 0..self.victim_queue.len() {
-            if self.victim_queue[i] == page_id {
-                self.victim_queue.remove(i);
-                self.pinned.remove(i);
-                self.pages_tracked.remove(&page_id);
-            }
-        }
+    fn notify_frame_unpin(&mut self, frame: &Frame) {}
+
+    fn notify_frame_destroy(&mut self, frame_id: u32) {
+        self.marked_for_eviction.remove(&frame_id);
     }
 }
